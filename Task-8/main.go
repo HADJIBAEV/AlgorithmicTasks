b87cd5dd704136e0167dfa6bf21b02ba6236d02f
Kahramon <hadjibaevkahramon@gmail.com>
2023-10-07 08:52:33 -0700

    Adding Task-8

--- Task-8/main.go
+++ Task-8/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		return
+	}
+	step := 1
+	N := 0
+	for n > step {
+		step *= 2
+		N++
+	}
+	fmt.Println(N)
 }
