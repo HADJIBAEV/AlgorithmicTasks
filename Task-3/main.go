d2c7c45405bbf05b81e771cb6284274a1691b36b
Kahramon <hadjibaevkahramon@gmail.com>
2023-10-20 05:55:24 -0700

    Adding Task-3

--- Task-3/main.go
+++ Task-3/main.go
@@ -1,39 +1,66 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"unicode"
+	"reflect"
+	"sort"
 )
 
-type Database struct {
-	m map[string]string
-}
-
-func NewDatabase() *Database {
-	return &Database{
-		m: make(map[string]string),
-	}
-}
-
 func main() {
-	str, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	ConvertRune := []rune(str)
-	if unicode.IsUpper(ConvertRune[0]) && string(ConvertRune[len(ConvertRune)-3]) == "." {
-		fmt.Println("Right")
-	} else {
-		fmt.Println("Wrong")
+	var NumberOfCard int
+	_, err := fmt.Scan(&NumberOfCard)
+	if err != nil {
+		return
 	}
-	//unicode.IsUpper(ConvertRune[0]) &&
-	//var str, oftenStr string
-	//fmt.Scan(&str)
-	//var count int
-	//k := make(map[string]int)
-	//for v := range str {
-	//	if v != count {
-	//		k = append(k, v)
-	//		k[v] = 0
-	//	}
-	//}
+
+	Subsequence := make([]int, NumberOfCard)
+	WinningSequence := make([]int, NumberOfCard)
+
+	for v := range Subsequence { // a1,a2, ... an
+		_, err2 := fmt.Scan(&Subsequence[v])
+		if err2 != nil {
+			return
+		}
+	}
+
+	for v := range WinningSequence { //b1,b2, ... bn
+		_, err2 := fmt.Scan(&WinningSequence[v])
+		if err2 != nil {
+			return
+		}
+	}
+
+	if CheckSlicesEquality(Subsequence, WinningSequence) {
+		fmt.Println("YES")
+		return
+	}
+
+	LeftBound, RightBound := FindLeftRightBound(NumberOfCard, Subsequence, WinningSequence) // Find Left Bound & Right Bound
+
+	sort.Ints(Subsequence[LeftBound : RightBound+1]) //sort Left Bound & Right Bound
+
+	if CheckSlicesEquality(Subsequence, WinningSequence) { // Subsequence == WinningSequence
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
+
+// FindLeftRightBound - Find Left bound & Right bound
+func FindLeftRightBound(n int, Subsequence, WinningSequence []int) (LeftBound int, RightBound int) {
+	for i := 0; i < n; i++ {
+		if Subsequence[i] != WinningSequence[i] {
+			if LeftBound == 0 {
+				LeftBound = i
+			} else {
+				RightBound = i
+			}
+		}
+	}
+	return LeftBound, RightBound
+}
+
+// CheckSlicesEquality - Check equality of slices
+func CheckSlicesEquality(Subsequence, WinningSequence []int) bool {
+	return reflect.DeepEqual(Subsequence, WinningSequence) // Subsequence == WinningSequence
 }
